14e27940d2c414e7f8c6207364f52635f29b21c1
vincentmuya <vincentmuya13@gmail.com>
2021-09-27 07:14:35 +0300

    created view function, url and template for onsale

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^sale$', views.on_sale, name='on_sale'),
 ]
 
 if settings.DEBUG:
--- project/app/views.py
+++ project/app/views.py
@@ -13,3 +13,13 @@ def index(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
     return render(request, "index.html", {'category': category,'categories': categories, 'products': products})
+
+
+def on_sale(request, category_slug=None):
+    category=None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request, "on_sale.html", {'category': category, 'categories': categories, 'products': products})
