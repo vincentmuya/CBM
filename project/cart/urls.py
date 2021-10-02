113a55be283bc0163d6da552a1d2aef6b970e7d6
vincentmuya <vincentmuya13@gmail.com>
2021-10-02 07:07:08 +0300

    created cart/urls and forms

---
+++ project/cart/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'cart'
+
+urlpatterns = [
+    url(r'^cart$', views.cart_detail, name='cart_detail'),
+    url(r'^add/(?P<product_id>\d+)/$',views.cart_add,name='cart_add'),
+    url(r'^remove/(?P<product_id>\d+)/$',views.cart_remove,name='cart_remove'),
+    url(r'^check_out$', views.check_out, name='check_out'),
+    url(r'^shipping_info$', views.buyer_info, name='buyer_info'),
+
+]
